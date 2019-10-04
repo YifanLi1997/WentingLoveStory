c0e855231e1b2f7a5773aaa20b85bf4053710524
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2019-10-04 10:41:17 +0100

    done

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI storyTMP;
+    [SerializeField] GameObject titleTMP;
+    [SerializeField] State startingState;
+
+    string imageCanvasName = "0";
+    GameObject imageCanvas;
+    State state;
+
+    int dimaxi = 0;
+    int huachenyu = 0;
+    int wangyibo = 0;
+    int xiaozhan = 0;
+    int wang_xiao = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+
+        UpdateStoryInfo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        for (int i = 0; i < nextStates.Length; i++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i)) {
+                state = nextStates[i];
+            }
+        }
+
+        UpdateStoryInfo();
+    }
+
+    private void UpdateStoryInfo()
+    {
+        storyTMP.SetText(state.GetStoryText());
+
+        if (state.GetIsTitle())
+        {
+            titleTMP.SetActive(true);
+        }
+        else
+        {
+            titleTMP.SetActive(false);
+        }
+
+        if (imageCanvasName != state.GetIimageCanvasName())
+        {
+            imageCanvas = GameObject.Find(imageCanvasName);
+            imageCanvas.GetComponent<CanvasGroup>().alpha = 0f;
+        }
+
+        imageCanvasName = state.GetIimageCanvasName();
+        imageCanvas = GameObject.Find(imageCanvasName);
+        imageCanvas.GetComponent<CanvasGroup>().alpha = 1f;
+
+        dimaxi += state.GetDi();
+        huachenyu += state.GetHua();
+        wangyibo += state.GetWang();
+        xiaozhan += state.GetXiao();
+        wang_xiao += state.GetWX();
+    }
+        
+        
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+    [SerializeField] string imageCanvasName;
+    [SerializeField] bool isTitle;
+    [SerializeField] int di;
+    [SerializeField] int hua;
+    [SerializeField] int wang;
+    [SerializeField] int xiao;
+    [SerializeField] int w_X;
+
+
+    public string GetStoryText() {
+        return storyText; 
+    }
+
+    public State[] GetNextStates() {
+        return nextStates;
+    }
+
+    public bool GetIsTitle()
+    {
+        return isTitle;
+    }
+
+    public string GetIimageCanvasName()
+    {
+        return imageCanvasName;
+    }
+
+    public int GetDi() {
+        return di;
+    }
+
+    public int GetHua()
+    {
+        return hua;
+    }
+
+    public int GetWang()
+    {
+        return wang;
+    }
+
+    public int GetXiao()
+    {
+        return xiao;
+    }
+
+    public int GetWX()
+    {
+        return w_X;
+    }
+}
